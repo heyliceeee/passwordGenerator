25ee2fae5b309b0aa04a2c11ef79dce18054cfcc
8200118 <58340504+heyliceeee@users.noreply.github.com>
2023-09-28 13:42:00 +0100

    :lipstick: feat: Estilização CSS dos tabs

--- passwordGenerator/src/routes.js
+++ passwordGenerator/src/routes.js
@@ -1,4 +1,5 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Ionicons} from '@expo/vector-icons'
 
 import { Home } from './pages/home'
 import { Passwords } from './pages/passwords'
@@ -9,8 +10,26 @@ const Tab = createBottomTabNavigator();
 export function Routes(){
     return (
         <Tab.Navigator>
-            <Tab.Screen name='home' component={Home}/>
-            <Tab.Screen name='passwords' component={Passwords}/>
+            <Tab.Screen name='home' component={Home} options={{headerShown: false, tabBarShowLabel: false,
+                tabBarIcon: ({focused, size, color }) => {
+                    if(focused){ //se a tab tiver selecionada
+                        return <Ionicons size={size} color={color} name='home'/>
+                    }
+
+                    //se a tab nao tiver selecionada
+                    return <Ionicons size={size} color={color} name='home-outline'/>
+                }
+            }}/>
+            <Tab.Screen name='passwords' component={Passwords} options={{headerShown: false, tabBarShowLabel: false,
+                tabBarIcon: ({focused, size, color }) => {
+                    if(focused){ //se a tab tiver selecionada
+                        return <Ionicons size={size} color={color} name='lock-closed'/>
+                    }
+
+                    //se a tab nao tiver selecionada
+                    return <Ionicons size={size} color={color} name='lock-closed-outline'/>
+                }
+            }}/>
         </Tab.Navigator>
     );
 }
